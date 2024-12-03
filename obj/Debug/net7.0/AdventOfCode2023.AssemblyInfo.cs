337b1067ce6b9fae51fa616883380f16b79cb260
OvsienkoValeriya <44091861+ovsienkovaleriya@users.noreply.github.com>
2024-12-03 12:15:05 +0100

    2024 - days 1-3

--- obj/Debug/net7.0/AdventOfCode2023.AssemblyInfo.cs
+++ obj/Debug/net7.0/AdventOfCode2023.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2023")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20851730ac920a23f3f8686a4af7fb65a610437d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2023")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2023")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
